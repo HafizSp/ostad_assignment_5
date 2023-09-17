90b98fbe754791128ead54158f677c82e6bdc638
HafizSp <hafizsp45@gmail.com>
2023-09-18 04:51:36 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,219 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(PhotoGalleryApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class PhotoGalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.add_alert_sharp),
-          title: Text(
-            'Home',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          actions: [
+      home: PhotoGallery(),
+    );
+  }
+}
+
+class PhotoGallery extends StatelessWidget {
+  MySnackBar(message, context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Photo Gallery"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: Icon(Icons.search),
-            )
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search for Photos...',
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: GridView.count(
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: [
+                  InkWell(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: 100,
+                            image: NetworkImage(
+                                'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-988013222-scaled-e1618857975729.jpg'),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text('Photo 0'),
+                      ],
+                    ),
+                    onTap: () {
+                      MySnackBar('Photo 0', context);
+                    },
+                  ),
+                  InkWell(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: 100,
+                            image: NetworkImage(
+                                'https://i.ytimg.com/vi/l7OznJMvldM/oar2.jpg?sqp=-oaymwEVCJUDENAFSFryq4qpAwcIARUAAIhC&rs=AOn4CLDH7DNnGaaQVS-YzMRG4A_FbBQ9KQ'),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text('Photo 1'),
+                      ],
+                    ),
+                    onTap: () {
+                      MySnackBar('Photo 1', context);
+                    },
+                  ),
+                  InkWell(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: 100,
+                            image: NetworkImage(
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJKZCojz9DCdBD6Q33rohTB9rmD0VUko8DzCjSEgAsCgQc3syYN3e3_dOeKbed4awRIPk&usqp=CAU'),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text('Photo 2'),
+                      ],
+                    ),
+                    onTap: () {
+                      MySnackBar('Photo 2', context);
+                    },
+                  ),
+                  InkWell(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: 100,
+                            image: NetworkImage(
+                                'https://e0.pxfuel.com/wallpapers/189/572/desktop-wallpaper-cute-baby-cat-baby-inocent-cute-cat-adorable-cat.jpg'),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text('Photo 3'),
+                      ],
+                    ),
+                    onTap: () {
+                      MySnackBar('Photo 3', context);
+                    },
+                  ),
+                  InkWell(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: 100,
+                            image: NetworkImage(
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJT75f8VxlvQnzxJKWcKMjRfBI7Gd_YiWq13kF-nlTdO5HhR1GMfr7tMqY9eneTXVDb1Y&usqp=CAU'),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text('Photo 4'),
+                      ],
+                    ),
+                    onTap: () {
+                      MySnackBar('Photo 4', context);
+                    },
+                  ),
+                  InkWell(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: 100,
+                            image: NetworkImage(
+                                'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_1280.jpg'),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text('Photo 5'),
+                      ],
+                    ),
+                    onTap: () {
+                      MySnackBar('Photo 5', context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg'),
+                ),
+                title: Text('Photo 1'),
+                subtitle: Text('Description for Photo 1'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://www.rd.com/wp-content/uploads/2019/11/cat-10-e1573844975155.jpg'),
+                ),
+                title: Text('Photo 2'),
+                subtitle: Text('Description for Photo 2'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://img.freepik.com/premium-photo/there-is-white-cat-with-blue-eyes-looking-up-generative-ai_902846-26861.jpg'),
+                ),
+                title: Text('Photo 3'),
+                subtitle: Text('Description for Photo 3'),
+              ),
+            ),
           ],
-          toolbarHeight: 100,
-          elevation: 70,
-          backgroundColor: Colors.green,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'This is mod 5 Assignment',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'My ',
-                  style: TextStyle(color: Colors.red, fontSize: 25),
-                  children: const <TextSpan>[
-                    TextSpan(
-                      text: 'phone ',
-                      style: TextStyle(color: Colors.blue, fontSize: 15),
-                    ),
-                    TextSpan(
-                      text: 'name ',
-                      style: TextStyle(
-                          color: Colors.purpleAccent,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: 'Your phone name',
-                      style: TextStyle(color: Colors.orange),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.upload),
+        onPressed: () {
+          MySnackBar('Photo Uploaded Successfully!', context);
+        },
       ),
     );
   }
